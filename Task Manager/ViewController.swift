64fb2e9383c539364b66995c10ce117d98dd8be5
Deepankar1484 <peterpro0123@gmail.com>
2024-11-13 10:03:52 +0530

    Initial Commit

---
+++ Task Manager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task Manager
+//
+//  Created by Deepankar Garg on 13/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Task ManagerTests/Task_ManagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Task_ManagerTests.swift
+//  Task ManagerTests
+//
+//  Created by Deepankar Garg on 13/11/24.
+//
+
+import Testing
+@testable import Task_Manager
+
+struct Task_ManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
